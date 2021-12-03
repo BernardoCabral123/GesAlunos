d39f7d1a9de724f085aa9b46541f7d029a8c6e06
bernardo <bernardo.cabral@outlook.com>
2021-12-03 12:37:20 -0100

    .

--- linux/src/model/routes/apiRoutes/alunosRoutes.js
+++ linux/src/model/routes/apiRoutes/alunosRoutes.js
@@ -21,14 +21,23 @@ router.get(`/`,(req,res)=>{
 });
 
 router.post(`/`,(req,res)=>{
-    dbConnection.query(`CALL criarAluno(${req.body.email},${req.body.password},${req.body.nome},${req.body.morada},${req.body.dataNascimento},${req.body.telemovel},${req.body.idTurma})`,(err,result)=>{
+
+    let newAluno = req.body;
+    let formattedDate = null;
+
+    if(newAluno.dataNascimento)
+        formattedDate = `'${newAluno.dataNascimento}'`;
+
+
+    dbConnection.query(`CALL criarAluno('${newAluno.email}','${newAluno.password}','${newAluno.nome}','${newAluno.morada}',${formattedDate},${newAluno.telemovel},${newAluno.idTurma})`,(err,result)=>{
         if(err){
-            console.log("falha na introducao do aluno")
+            console.log("falha na introducao do aluno");
         }
         else{
-            res.json(result);
+            res.send("deu certo");
         }
   });
+});
 
 
 
